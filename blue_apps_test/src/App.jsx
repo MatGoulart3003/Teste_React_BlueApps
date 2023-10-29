14cf824e242ca339bbcfe14e2e6b1aff96583c58
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2023-10-29 16:49:51 -0300

    Adicionada função de Criar, editar e excluir anotações

--- blue_apps_test/src/App.jsx
+++ blue_apps_test/src/App.jsx
@@ -1,23 +1,58 @@
+/* eslint-disable react/no-array-index-key */
 import { useState } from "react"
 
 function App() {  
 
-  const [title, setTitle] = useState ()
-  const [anotation, setAnotation] = useState ();
+  const [title, setTitle] = useState ('')
+  const [anotation, setAnotation] = useState ('')
   const [createAnotationButton, setCreateAnotationButton] = useState (false)
   const [anotationList, setAnotationList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
 
   const handleCreateAnotationButton = () => {
     setCreateAnotationButton(!createAnotationButton)
   }
 
   const handleTitleChange = (event) => {
-    setTitle(event.target.value);
+    setTitle(event.target.value)
   };
 
   const handleDescricaoChange = (event) => {
-    setAnotation(event.target.value);
+    setAnotation(event.target.value)
   };
+
+  const addAnotation = () => {
+    if (title && anotation) {
+      const newAnotation = { title, anotation }
+      if (isEditing){
+        setAnotationList([...anotationList, newAnotation]);
+        setIsEditing(false);
+      }
+      setAnotationList([...anotationList, newAnotation])
+      setTitle('')
+      setAnotation('')
+      handleCreateAnotationButton()
+    } else {
+      alert('Por favor, preencha o título e a descrição.')
+    }
+  };
+  
+  const deleteAnotation = (index) => {
+    const auxList = [...anotationList];
+    auxList.splice(index, 1);
+    setAnotationList(auxList);
+  };
+
+  const editAnotation = (index) => {
+    const anotationToEdit = anotationList[index]
+    setTitle(anotationToEdit.title)
+    setAnotation(anotationToEdit.anotation)
+    setIsEditing(true)
+    deleteAnotation(index)
+    handleCreateAnotationButton()
+  };
+
+  
 
   return (
     <div>
@@ -28,22 +63,29 @@ function App() {
     
      {createAnotationButton && (
       <form>
-        Criação de anotação:
         <label>
-          Titulo: <input type="text" value={title} onChange={handleTitleChange}/>          
+          Titulo:
+          <input type="text" value={title} onChange={handleTitleChange}/>          
         </label>
         <label>
-          Descrição: <input type="text" value={anotation} onChange={handleDescricaoChange} />
-        </label>
-        <button type="button" onClick={handleCreateAnotationButton}> Salvar </button>
-
+          Descrição:
+          <input type="text" value={anotation} onChange={handleDescricaoChange} />
+        </label>       
+            <button type="button" onClick={addAnotation}> Salvar </button>   
       </form>
      )}
 
-      <div>
-        <h1> {title} </h1>
-        <h1> {anotation} </h1>
-      </div>
+    <h2>Lista de Anotações</h2>
+      <ul>
+        {anotationList.map((item, index) => (
+          <li key={index}>
+            <strong>{item.title}</strong>: {item.anotation}
+            <button type="button" onClick={() => editAnotation(index)}> Editar </button>
+            <button type="button" onClick={() => deleteAnotation(index)}> Remover </button>
+          </li>
+          
+        ))}
+      </ul>
 
     </div>
   )
